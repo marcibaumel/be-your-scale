06faeb15b63bd5724da5672f4533f20ef690a798
Baumel Márton <marcibaumel@gmail.com>
2026-07-18 13:02:13 +0200

    base camera movement

--- beYourScale/scripts/player.gd
+++ beYourScale/scripts/player.gd
@@ -1,31 +1,47 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var SPEED: float = 5.0
+var JUMP_VELOCITY: float = 4.5
+var GRAVITY:float = 9.68
+var CAMERA_SENSITIVITY = 0.005
 
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * CAMERA_SENSITIVITY)
+		camera.rotate_x(-event.relative.y * CAMERA_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
 	if not is_on_floor():
-		print("not on the floor")
 		velocity += get_gravity() * delta
 
+	# Handle exit
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		print(input_dir)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0.0
+		velocity.z = 0.0
 
 	move_and_slide()
