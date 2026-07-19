e9fb2c143fa838551f197b27d703ddbfc842d2ab
Baumel Márton <marcibaumel@gmail.com>
2026-07-19 10:23:51 +0200

    update workspace for vs code

--- beYourScale/scripts/player.gd
+++ beYourScale/scripts/player.gd
@@ -42,20 +42,15 @@ func _headbob(time) -> Vector3:
 	return pos
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle exit
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
 		
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
